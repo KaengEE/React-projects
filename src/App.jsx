4dfca23932bd8c0c98685b2594a0cc4629042550
KaengEE <yopuiqwer@naver.com>
2023-11-29 11:22:05 +0900

    초기화

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <>프로젝트</>;
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+import p1 from './assets/birthday.png';
+import p2 from './assets/tours.png';
+import p3 from './assets/reviews.png';
+import p4 from './assets/questions.png';
+
+
+export const projects = [
+  {
+    id: 1,
+    title: 'birthday buddy',
+    url: 'https://react-vite-projects-1-birthday-buddy.netlify.app/',
+    image: p1,
+  },
+  {
+    id: 2,
+    title: 'tours',
+    url: 'https://react-vite-projects-2-tours.netlify.app/',
+    image: p2,
+  },
+  {
+    id: 3,
+    title: 'reviews',
+    url: 'https://react-vite-projects-3-reviews.netlify.app/',
+    image: p3,
+  },
+  {
+    id: 4,
+    title: 'questions',
+    url: 'https://react-vite-projects-4-accordion.netlify.app/',
+    image: p4,
+  },
+];
