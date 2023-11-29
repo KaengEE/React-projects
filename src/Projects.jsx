36f57edbb95bc0489f2a0d0b159dea187896c731
KaengEE <yopuiqwer@naver.com>
2023-11-29 11:41:08 +0900

    프로젝트 컴포넌트 설정 및 샘플이미지 추가

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,5 +1,51 @@
 import React from "react";
+import p1 from "./assets/p1.png";
 
 export const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <section className="projects">
+      <div className="title">
+        <h2>projects</h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="projects-center">
+        <a
+          href="./assets/birthday.png"
+          target="_blank"
+          rel="noreferrer"
+          className="project"
+        >
+          <img src={p1} alt="birthday buddy" className="img" />
+          <h5>birthday buddy</h5>
+        </a>
+        <a
+          href="./assets/birthday.png"
+          target="_blank"
+          rel="noreferrer"
+          className="project"
+        >
+          <img src={p1} alt="birthday buddy" className="img" />
+          <h5>birthday buddy</h5>
+        </a>
+        <a
+          href="./assets/birthday.png"
+          target="_blank"
+          rel="noreferrer"
+          className="project"
+        >
+          <img src={p1} alt="birthday buddy" className="img" />
+          <h5>birthday buddy</h5>
+        </a>
+        <a
+          href="./assets/birthday.png"
+          target="_blank"
+          rel="noreferrer"
+          className="project"
+        >
+          <img src={p1} alt="birthday buddy" className="img" />
+          <h5>birthday buddy</h5>
+        </a>
+      </div>
+    </section>
+  );
 };
